c5a4cacb3a451f8cd14a92431f2466331f2f5f47
Erm-Miras <miras47kz@gmail.com>
2026-01-31 09:24:36 +0500

    Add Practice1 Python basics examples

---
+++ practice 1/.vscode/datatype.py
@@ -0,0 +1,17 @@
+# 1-мысал: Мәтіндік тип (str)
+x = "Hello"
+
+# 2-мысал: Сандық типтер (int, float)
+y = 20      # int
+z = 20.5    # float
+
+# 3-мысал: Тізімдер (list, tuple, dict)
+fruits = ["apple", "banana"] # list
+colors = ("red", "green")    # tuple
+
+# 4-мысал: Логикалық тип (bool)
+is_active = True
+
+# 5-мысал: Типті анықтау (type функциясы)
+print(type(x))
+print(type(y))
